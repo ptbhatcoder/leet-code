6743d5358ff7bf89554ffd919cad331970051951
Pranav Thirthahalli Bhat <ptbhatcoder@users.noreply.github.com>
2025-02-03 06:56:38 +0530

    Time: 1 ms (83.93%), Space: 51.5 MB (57.14%) - LeetHub

--- 3372-longest-strictly-increasing-or-strictly-decreasing-subarray/3372-longest-strictly-increasing-or-strictly-decreasing-subarray.js
+++ 3372-longest-strictly-increasing-or-strictly-decreasing-subarray/3372-longest-strictly-increasing-or-strictly-decreasing-subarray.js
@@ -3,28 +3,21 @@
  * @return {number}
  */
 var longestMonotonicSubarray = function(nums) {
-    let maxInc = 1, inc = 1, maxDec = 1, dec = 1;
-    for(let i = 1; i < nums.length; i++){
+    let max = 1;
+    for(let i = 1, inc = 1, dec = 1; i < nums.length; i++){
         const cur = nums[i];
         const prev = nums[i-1];
         if(cur < prev){
-            maxInc = Math.max(maxInc, inc);
             inc = 1;
             dec++;
         } else if(prev < cur) {
-            maxDec = Math.max(maxDec, dec);
             dec = 1;
             inc++;
         } else {
-            maxInc = Math.max(maxInc, inc);
-            maxDec = Math.max(maxDec, dec);
             inc = 1;
             dec = 1;
         }
-        if(i === nums.length - 1){
-            maxInc = Math.max(maxInc, inc);
-            maxDec = Math.max(maxDec, dec);
-        }
+        max = Math.max(max, inc, dec);
     }
-    return Math.max(maxInc, maxDec);
+    return max;
 };
