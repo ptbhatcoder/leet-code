b70d247a91bfb4e954a81e9fdb120a3f3a9719d3
Pranav Thirthahalli Bhat <ptbhatcoder@users.noreply.github.com>
2025-02-20 07:25:14 +0530

    Time: 0 ms (100%), Space: 53.1 MB (21.13%) - LeetHub

--- 2107-find-unique-binary-string/2107-find-unique-binary-string.js
+++ 2107-find-unique-binary-string/2107-find-unique-binary-string.js
@@ -3,21 +3,10 @@
  * @return {string}
  */
 var findDifferentBinaryString = function(nums) {
-    const n = nums[0].length;
-    const present = new Set(nums.map(num => {
-        let val = 0;
-        for(const c of num){
-            val <<= 1;
-            val = val | (c === '0' ? 0 : 1);
-        }
-        return val;
-    }));
-    let cur = (1 << n) - 1;
-    while(present.has(cur)) cur--;
-    const result = Array.from({ length: n }, _ => 0);
-    for(let i = n - 1; i >= 0 && cur; i--){
-        result[i] = cur & 1;
-        cur >>= 1;
+    const n = nums.length;
+    const result = new Array(n).fill(0);
+    for(let i = 0; i < n; i++){
+        result[i] = nums[i][i] === '0' ? 1 : 0;
     }
     return result.join('');
 };
