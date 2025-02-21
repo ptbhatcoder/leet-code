1710557cd048e30e14e179ebcf0d6b90651ce9ec
Pranav Thirthahalli Bhat <ptbhatcoder@users.noreply.github.com>
2025-02-21 06:12:23 +0530

    Time: 9 ms (68%), Space: 65.7 MB (8%) - LeetHub

---
+++ 1387-find-elements-in-a-contaminated-binary-tree/1387-find-elements-in-a-contaminated-binary-tree.js
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ */
+var FindElements = function(root) {
+    this.lookup = new Map;
+    const inorder = (node, val) => {
+        if(!node) return;
+        const twice = 2 * val;
+        inorder(node.left, twice + 1);
+        node.val = val;
+        this.lookup.set(val, node);
+        inorder(node.right, twice + 2);
+    }
+    inorder(root, 0);
+};
+
+/** 
+ * @param {number} target
+ * @return {boolean}
+ */
+FindElements.prototype.find = function(target) {
+    return this.lookup.has(target);
+};
+
+/** 
+ * Your FindElements object will be instantiated and called as such:
+ * var obj = new FindElements(root)
+ * var param_1 = obj.find(target)
+ */
