1659c7c80ed7d1b24c6f992cb32292d2f4ab655c
Pranav Thirthahalli Bhat <ptbhatcoder@users.noreply.github.com>
2025-02-25 07:52:57 +0530

    Time: 12 ms (17.86%), Space: 67.9 MB (7.14%) - LeetHub

--- 1631-number-of-sub-arrays-with-odd-sum/1631-number-of-sub-arrays-with-odd-sum.js
+++ 1631-number-of-sub-arrays-with-odd-sum/1631-number-of-sub-arrays-with-odd-sum.js
@@ -6,7 +6,7 @@ const MOD = 7 + 10 ** 9;
 var numOfSubarrays = function(arr) {
     let evens = 1, odds = 0, current = 0;
     for(const num of arr){
-        current = (current % MOD + num % MOD) % MOD;
+        current = current + (num % 2);
         if(current & 1) odds = (odds % MOD + 1) % MOD;
         else evens = (evens % MOD + 1) % MOD;
     }
