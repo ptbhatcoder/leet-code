7d9fea6ac927202185b63b918c7a2e4cca37b54c
Pranav Thirthahalli Bhat <ptbhatcoder@users.noreply.github.com>
2025-02-26 07:46:38 +0530

    Time: 2 ms (85.37%), Space: 60.5 MB (19.51%) - LeetHub

---
+++ 1849-maximum-absolute-sum-of-any-subarray/1849-maximum-absolute-sum-of-any-subarray.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxAbsoluteSum = function(nums) {
+    let pos = 0, neg = 0, max = 0;
+    for(const num of nums){
+        pos += num;
+        neg += num;
+        if(pos < 0) pos = 0;
+        if(neg > 0) neg = 0;
+        max = Math.max(max, pos, Math.abs(neg), Math.abs(num));
+    }
+    return max;
+};
