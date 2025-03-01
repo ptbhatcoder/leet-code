46128601a0548d273dfa2a6e43c1f268e8e25d1b
Pranav Thirthahalli Bhat <ptbhatcoder@users.noreply.github.com>
2025-03-01 09:38:31 +0530

    Time: 1 ms (87.25%), Space: 55.5 MB (16.67%) - LeetHub

--- 2551-apply-operations-to-an-array/2551-apply-operations-to-an-array.js
+++ 2551-apply-operations-to-an-array/2551-apply-operations-to-an-array.js
@@ -5,20 +5,19 @@
 var applyOperations = function(nums) {
     const n = nums.length;
     const ops = n - 1;
-    const res = [...nums];
     for(let i = 0; i < ops; i++){
         if(nums[i] === nums[i+1]){
-            res[i] = 2 * nums[i];
+            nums[i] = 2 * nums[i];
             i++;
-            res[i] = 0;
-        } else res[i] = nums[i];
+            nums[i] = 0;
+        };
     }
     let idx = 0;
     for(let i = 0; i < n; i++){
-        if(res[i] > 0){
-            res[idx++] = res[i];
+        if(nums[i] > 0){
+            nums[idx++] = nums[i];
         }
     }
-    while(idx < n) res[idx++] = 0;
-    return res;
+    while(idx < n) nums[idx++] = 0;
+    return nums;
 };
