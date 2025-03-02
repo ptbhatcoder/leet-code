d57f9b6b4a1a7f3495b36f509b18d6f802c5b727
Pranav Thirthahalli Bhat <ptbhatcoder@users.noreply.github.com>
2025-03-02 14:17:50 +0530

    Time: 4 ms (61.84%), Space: 59.2 MB (14.47%) - LeetHub

--- 2707-merge-two-2d-arrays-by-summing-values/2707-merge-two-2d-arrays-by-summing-values.js
+++ 2707-merge-two-2d-arrays-by-summing-values/2707-merge-two-2d-arrays-by-summing-values.js
@@ -4,13 +4,14 @@
  * @return {number[][]}
  */
 var mergeArrays = function(nums1, nums2) {
-    const result = [];
     let i = 0, j  = 0;
     const n1 = nums1.length, n2 = nums2.length;
+    const result = new Array(n1 + n2);
+    let idx = -1;
     const insert = (elem) => {
-        if(result.length > 0 && result.at(-1)[0] === elem[0]){
-            result.at(-1)[1] += elem[1];
-        } else result.push(elem);
+        if(idx >= 0 && result[idx][0] === elem[0]){
+            result[idx][1] += elem[1];
+        } else result[++idx] = elem;
     }
     while(i < n1 && j < n2){
         if(nums1[i][0] < nums2[j][0]) insert(nums1[i++]);
@@ -18,5 +19,5 @@ var mergeArrays = function(nums1, nums2) {
     }
     while(i < n1) insert(nums1[i++]);
     while(j < n2) insert(nums2[j++]);
-    return result;
+    return result.slice(0, 1 + idx);
 };
