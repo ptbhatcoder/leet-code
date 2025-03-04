8352582b9af73a4753abc6d5abfa1c650045cd65
Pranav Thirthahalli Bhat <ptbhatcoder@users.noreply.github.com>
2025-03-04 09:11:51 +0530

    Time: 0 ms (100%), Space: 53.8 MB (18.18%) - LeetHub

--- 1889-check-if-number-is-a-sum-of-powers-of-three/1889-check-if-number-is-a-sum-of-powers-of-three.js
+++ 1889-check-if-number-is-a-sum-of-powers-of-three/1889-check-if-number-is-a-sum-of-powers-of-three.js
@@ -1,14 +1,21 @@
+const ALLOWED = [0,1];
+const sumsPowersOf = base => {
+    return target => {
+        while(target){
+            const den = target % base;
+            if(!ALLOWED.includes(den)) return false;
+            target -= den;
+            target /= base;
+        }
+        return true;
+    }
+}
+
+const isSumOfPowersOfThree = sumsPowersOf(3);
 /**
  * @param {number} n
  * @return {boolean}
  */
 var checkPowersOfThree = function(n) {
-    let val = n;
-    while(val){
-        const den = val % 3;
-        if(den === 2) return false;
-        val -= den;
-        val /= 3;
-    }
-    return true;
+    return isSumOfPowersOfThree(n);
 };
