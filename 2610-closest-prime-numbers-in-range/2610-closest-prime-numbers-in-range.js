53c0a101c954401b97fe397745edd10d6255e521
Pranav Thirthahalli Bhat <ptbhatcoder@users.noreply.github.com>
2025-03-07 07:06:48 +0530

    Time: 2 ms (100%), Space: 64.2 MB (33.33%) - LeetHub

---
+++ 2610-closest-prime-numbers-in-range/2610-closest-prime-numbers-in-range.js
@@ -0,0 +1,59 @@
+
+const generatePrimes = (ceil = 10**6) => {
+    const sieve = new Array(1 + ceil).fill(true);
+    sieve[0] = false;
+    sieve[1] = false;
+    const l = sieve.length;
+    let count = 0;
+    for(let cur = 2; cur < l; cur++){
+        if(sieve[cur] === false) continue;
+        count++;
+        let val = cur + cur;
+        while(val < l){
+            sieve[val] = false;
+            val += cur;
+        }
+    }
+    const primes = new Array(count);
+    let idx = 0;
+    for(let cur = 2; cur < l; cur++){
+        if(sieve[cur]) primes[idx++] = cur;
+    }
+    return primes;
+}
+
+const primes = generatePrimes();
+const len = primes.length;
+const findCeil = val => {
+    let s = 0, e = len - 1;
+    let res = -1;
+    while(s <= e){
+        const mid = s + ((e - s) >> 1);
+        if(primes[mid] >= val){
+            res = mid;
+            e = mid - 1;
+        } else s = mid + 1;
+    }
+    return res;
+}
+
+/**
+ * @param {number} left
+ * @param {number} right
+ * @return {number[]}
+ */
+var closestPrimes = function(left, right) {
+    const idx = findCeil(left);
+    if(idx < 0) return [-1, -1];
+    let num1 = -1, num2 = -1;
+    let diff = Infinity;
+    for(let i = idx; i + 1 < len && primes[i+1] <= right; i++){
+        const cur = primes[i+1] - primes[i];
+        if(cur < diff){
+            num1 = primes[i];
+            num2 = primes[i+1];
+            diff = cur;
+        }
+    }
+    return [num1, num2];
+};
