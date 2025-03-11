a37b8d743531a60cc9edb81d21756913aa94a88e
Pranav Thirthahalli Bhat <ptbhatcoder@users.noreply.github.com>
2025-03-11 18:21:49 +0530

    Time: 7 ms (96.85%), Space: 56.6 MB (24.02%) - LeetHub

--- 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.js
+++ 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.js
@@ -3,41 +3,25 @@
  * @return {number}
  */
 var numberOfSubstrings = function(s) {
-    let a = 0, b = 0, c = 0;
+    let a = -1, b = -1, c = -1;
     const n = s.length;
-    let res = 0, j = 0;
+    let res = 0;
     for(let i = 0; i < n; i++){
         switch(s[i]){
             case 'a':
-                a++;
+                a = i;
                 break;
             case 'b':
-                b++;
+                b = i;
                 break;
             case 'c':
-                c++;
+                c = i;
                 break;
             default:
                 break;
             
         }
-        while(a > 0 && b > 0 && c > 0){
-            switch(s[j]){
-                case 'a':
-                    a--;
-                    break;
-                case 'b':
-                    b--;
-                    break;
-                case 'c':
-                    c--;
-                    break;
-                default:
-                    break;
-            }
-            j++;
-            res += (n - i)
-        }
+        if(a > -1 && b > -1 && c > -1) res += (Math.min(a, b, c) + 1);
     }
     return res;
 };
