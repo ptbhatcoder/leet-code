e5439d14290f61384b8a421852e26595397c4ad2
Pranav Thirthahalli Bhat <ptbhatcoder@users.noreply.github.com>
2025-03-14 09:04:49 +0530

    Time: 34 ms (48.39%), Space: 68.1 MB (25.81%) - LeetHub

--- 1335-maximum-candies-allocated-to-k-children/1335-maximum-candies-allocated-to-k-children.js
+++ 1335-maximum-candies-allocated-to-k-children/1335-maximum-candies-allocated-to-k-children.js
@@ -1,24 +1,22 @@
+const UPPER = 10**7;
 /**
  * @param {number[]} candies
  * @param {number} k
  * @return {number}
  */
 var maximumCandies = function(candies, k) {
-    let max = -Infinity, total = 0;
-    for(const candy of candies){
-        max = Math.max(max, candy);
-        total += candy;
+    let low = 1, high = UPPER;
+    let ans = 0;
+    while(low <= high){
+        const mid = low + ((high - low) >> 1);
+        let sets = 0;
+        for(const candy of candies){
+            sets += Math.floor(candy / mid);
+        }
+        if(sets >= k){
+            low = mid + 1;
+            ans = mid;
+        } else high = mid - 1;
     }
-    if(total < k) return 0;
-    let s = 1, e = max, res = 0;
-    const getPileCount = (size) => candies.reduce((acc, candy) => acc + Math.floor(candy / size),0);
-    while(s <= e){
-        const mid = s + ((e - s) >> 1);
-        const pileCount = getPileCount(mid);
-        if(pileCount >= k){
-            res = mid;
-            s = mid + 1;
-        } else e = mid - 1;
-    }
-    return res;
+    return ans;
 };
