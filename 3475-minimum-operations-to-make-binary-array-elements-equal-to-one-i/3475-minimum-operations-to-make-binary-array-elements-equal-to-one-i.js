711c4b2c55f7d3575dc77363aabe4308970a45ec
Pranav Thirthahalli Bhat <ptbhatcoder@users.noreply.github.com>
2025-03-19 08:36:25 +0530

    Time: 6 ms (80%), Space: 65.5 MB (5.45%) - LeetHub

--- 3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i.js
+++ 3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i.js
@@ -4,27 +4,18 @@
  */
 var minOperations = function(nums) {
     const n = nums.length;
-    let last = n - 3;
-    let count = 0;
-    for(const num of nums){
-        if(num === 0) count++;
-    };
+    const last = n - 3;
     let flips = 0;
-    for(let i = 0; count > 0 && i <= last; i++){
-        if(nums[i] === 1) continue;
-        flips++;
-        for(let j = 0; j < 3; j++){
-            let val = nums[i + j];
-            if(val === 0){
-                count--;
-                val = 1;
-            } else {
-                count++;
-                val = 0;
-            }
-            nums[i + j] = val;
+    const flip = i => {
+        nums[i] = (1 + nums[i]) % 2;
+    }
+    for(let i = 0; i <= last; i++){
+        if(nums[i] === 0){
+            flips++;
+            flip(i);
+            flip(i+1);
+            flip(i+2);
         }
     }
-
-    return count > 0 ? -1 : flips;
+    return nums.at(-1) === 1 && nums.at(-2) === 1 ? flips : -1;
 };
