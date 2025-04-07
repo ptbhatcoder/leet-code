640d9910cef70eec4a5f3d54d48d110171af4449
Pranav Thirthahalli Bhat <ptbhatcoder@users.noreply.github.com>
2025-04-07 07:44:28 +0530

    Time: 157 ms (46.87%), Space: 87.2 MB (33.65%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.js
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.js
@@ -4,15 +4,16 @@
  */
 var canPartition = function(nums) {
     const n = nums.length;
-    const total = nums.reduce((s, a) => s + a);
+    let total = 0;
+    for(const num of nums) total += num;
     if(total & 1) return false;
 
-    const target = total >> 1;
-    const m = 1 + target;
-    const dp = Array.from({ length: n + 1 }, _ => new Array(m).fill(false));
+    total /= 2;
+    total++;
+    const dp = Array.from({ length: n + 1 }, _ => new Array(total).fill(false));
     dp[0][0] = true;
     for(let i = 1; i <= n; i++){
-        for(let j = 0; j < m; j++){
+        for(let j = 0; j < total; j++){
             dp[i][j] = dp[i-1][j];
             if(j >= nums[i-1]){
                 dp[i][j] ||= dp[i-1][j-nums[i-1]];
