89f3d86dbca79429012493b37b1ee1cc25b9bcf4
Pranav Thirthahalli Bhat <ptbhatcoder@users.noreply.github.com>
2025-04-09 05:48:42 +0530

    Time: 55 ms (69.39%), Space: 57.2 MB (32.65%) - LeetHub

--- 3621-minimum-operations-to-make-array-values-equal-to-k/3621-minimum-operations-to-make-array-values-equal-to-k.js
+++ 3621-minimum-operations-to-make-array-values-equal-to-k/3621-minimum-operations-to-make-array-values-equal-to-k.js
@@ -5,7 +5,6 @@
  */
 var minOperations = function(nums, k) {
     const gt = new Set;
-    let cK = 0, hasLess = false;
     for(const num of nums){
         if(num < k) return -1;
         if(num > k) gt.add(num);
