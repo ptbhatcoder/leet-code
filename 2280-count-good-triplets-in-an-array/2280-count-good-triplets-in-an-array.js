cf506d66335709a67be5f29b81e27aed6d480117
Pranav Thirthahalli Bhat <ptbhatcoder@users.noreply.github.com>
2025-04-15 06:03:10 +0530

    Time: 37 ms (100%), Space: 70.2 MB (85.71%) - LeetHub

--- 2280-count-good-triplets-in-an-array/2280-count-good-triplets-in-an-array.js
+++ 2280-count-good-triplets-in-an-array/2280-count-good-triplets-in-an-array.js
@@ -1,45 +1,50 @@
+class FenwickTree {
+    constructor(size) {
+        this.tree = new Array(size + 1).fill(0);
+    }
+
+    update(index, delta) {
+        index++;
+        while (index < this.tree.length) {
+            this.tree[index] += delta;
+            index += index & -index;
+        }
+    }
+
+    query(index) {
+        index++;
+        let res = 0;
+        while (index > 0) {
+            res += this.tree[index];
+            index -= index & -index;
+        }
+        return res;
+    }
+}
+
 /**
  * @param {number[]} nums1
  * @param {number[]} nums2
  * @return {number}
  */
-function goodTriplets(nums1, nums2) {
-  const elemToIndexMappingInB = new Map();
-  const n = nums1.length;
-  const segmentTree = new Array(n * 4 + 1).fill(0);
-  let ans = 0;
-  for (let i = 0; i < nums2.length; i++) {
-    elemToIndexMappingInB.set(nums2[i], i);
-  }
-  update(segmentTree, 1, 0, n - 1, elemToIndexMappingInB.get(nums1[0]));
-  for (let i = 1; i < n; i++) {
-    const indexInB = elemToIndexMappingInB.get(nums1[i]);
-    const commonElementsOnLeft = query(segmentTree, 1, 0, n - 1, 0, indexInB);
-    const uniqueElementsOnLeftInA = i - commonElementsOnLeft;
-    const elementsAfterIndexInB = n - 1 - indexInB;
-    const commonElementsOnRight = elementsAfterIndexInB - uniqueElementsOnLeftInA;
-    ans += commonElementsOnLeft * commonElementsOnRight;
-    update(segmentTree, 1, 0, n - 1, indexInB);
-  }
-  return ans;
-}
-
-function update(st, index, start, end, updateIndex) {
-  if (start == end) {
-    st[index] += 1;
-    return;
-  }
-  const mid = start + Math.floor((end - start) / 2);
-  if (updateIndex <= mid) update(st, index * 2, start, mid, updateIndex);
-  else update(st, index * 2 + 1, mid + 1, end, updateIndex);
-  st[index] = st[index * 2] + st[index * 2 + 1];
-}
-
-function query(st, index, start, end, queryStart, queryEnd) {
-  if (end < queryStart || start > queryEnd) return 0;
-  if (start >= queryStart && end <= queryEnd) return st[index];
-  const mid = start + Math.floor((end - start) / 2);
-  const left = query(st, index * 2, start, mid, queryStart, queryEnd);
-  const right = query(st, index * 2 + 1, mid + 1, end, queryStart, queryEnd);
-  return left + right;
-}
+var goodTriplets = function (nums1, nums2) {
+    const n = nums1.length;
+    const pos2 = new Array(n),
+        reversedIndexMapping = new Array(n);
+    for (let i = 0; i < n; i++) {
+        pos2[nums2[i]] = i;
+    }
+    for (let i = 0; i < n; i++) {
+        reversedIndexMapping[pos2[nums1[i]]] = i;
+    }
+    const tree = new FenwickTree(n);
+    let res = 0;
+    for (let value = 0; value < n; value++) {
+        const pos = reversedIndexMapping[value];
+        const left = tree.query(pos);
+        tree.update(pos, 1);
+        const right = n - 1 - pos - (value - left);
+        res += left * right;
+    }
+    return res;
+};
