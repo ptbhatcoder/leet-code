b9f86ec43e5ffc4a20ab7a3910f749aa1b5b433d
Pranav Thirthahalli Bhat <ptbhatcoder@users.noreply.github.com>
2025-04-26 08:28:38 +0530

    Time: 7 ms (77.78%), Space: 65.3 MB (85.19%) - LeetHub

--- 2527-count-subarrays-with-fixed-bounds/2527-count-subarrays-with-fixed-bounds.js
+++ 2527-count-subarrays-with-fixed-bounds/2527-count-subarrays-with-fixed-bounds.js
@@ -4,24 +4,16 @@
  * @param {number} maxK
  * @return {number}
  */
-var countSubarrays = function (nums, minK, maxK) {
-    let x = -1;
-    let y = -1;
-    let z = -1;
-    let p = 0;
-    let total = 0;
-
-    for (let = 0; p < nums.length; p++) {
-        if (nums[p] === minK) {
-            x = p;
-        }
-        if (nums[p] === maxK) {
-            y = p;
-        }
-        if (nums[p] > maxK || nums[p] < minK) {
-            z = p;
-        }
-        total += Math.max(Math.min(x,y) - z, 0);
+var countSubarrays = function(nums, minK, maxK) {
+    let bad = -1, iMin = -1, iMax = -1;
+    let res = 0;
+    const n = nums.length;
+    for(let r = 0; r < n; r++){
+        const num =  nums[r];
+        if(num < minK || num > maxK) bad = r;
+        if(num === minK) iMin = r;
+        if(num === maxK) iMax = r;
+        res += Math.max(0, Math.min(iMin, iMax) - bad);
     }
-    return total;
+    return res;
 };
