f901c11e45d1c70dc1137c0bb56b277ce315d1f5
Pranav Thirthahalli Bhat <ptbhatcoder@users.noreply.github.com>
2025-05-02 06:59:28 +0530

    Time: 43 ms (58.33%), Space: 67.3 MB (72.22%) - LeetHub

--- 0868-push-dominoes/0868-push-dominoes.js
+++ 0868-push-dominoes/0868-push-dominoes.js
@@ -4,25 +4,41 @@
  */
 var pushDominoes = function(dominoes) {
     const n = dominoes.length;
-    const balance = Array.from({ length: n }, _ => 0);
+    const chars = {};
+    const indices = {};
     
-    let runner = 0;
+    let nSymbols = 1;
+    chars[0] = 'L';
+    indices[0] = -1;
     for(let i = 0; i < n; i++){
-        if(dominoes[i] === 'R'){
-            runner = n;
-        } else if(dominoes[i] === 'L'){
-            runner = 0;
-        } else if(runner > 0) runner--;
-        balance[i] += runner;
+        if(dominoes[i] !== '.'){
+            chars[nSymbols] = dominoes[i];
+            indices[nSymbols] = i;
+            nSymbols++;
+        }
+    }
+    chars[nSymbols] = 'R';
+    indices[nSymbols++] = n;
+    const ans = [...dominoes];
+    for(let ind = 0; ind < nSymbols - 1; ind++){
+        const i = indices[ind], j = indices[ind+1];
+        const x = chars[ind], y = chars[ind+1];
+       
+        if(x === y){
+           
+            for(let k = i + 1; k < j; k++){
+                ans[k] = x;
+            }
+        } else if(x > y){
+            for(let k = i + 1; k < j ; k++){
+                if(k - i < j - k){
+                    ans[k] = x;
+                } else if(k - i > j - k){
+                    ans[k] = y;
+                }  else ans[k] = '.'
+            }
+        }
     }
     
-    runner = 0;
-    for(let i = n - 1; i >= 0; i--){
-        if(dominoes[i] === 'L') runner = -n;
-        else if(dominoes[i] === 'R') runner = 0;
-        else if(runner < 0) runner++;
-        balance[i]+= runner;
-    }
-    
-    return balance.map(i => i < 0 ? 'L' : i === 0 ? '.' : 'R').join('');
+    return ans.join('');
 };
