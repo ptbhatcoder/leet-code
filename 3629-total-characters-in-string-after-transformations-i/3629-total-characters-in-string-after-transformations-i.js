0695fb7d76e6792e189fda42358e3c5eb617ad26
Pranav Thirthahalli Bhat <ptbhatcoder@users.noreply.github.com>
2025-05-13 07:46:44 +0530

    Time: 342 ms (50%), Space: 64 MB (62.5%) - LeetHub

--- 3629-total-characters-in-string-after-transformations-i/3629-total-characters-in-string-after-transformations-i.js
+++ 3629-total-characters-in-string-after-transformations-i/3629-total-characters-in-string-after-transformations-i.js
@@ -1,31 +1,21 @@
-const MOD = 7 + 10**9;
-const TEMPLATE = 'abcdefghijklmnopqrstuvwxyz';
-const FREQ =  Object.fromEntries([...TEMPLATE].map(k => [k, 0]));
-const getNext = k => String.fromCharCode(k.charCodeAt(0) + 1);
+const MOD = 7 + 10 ** 9;
 /**
  * @param {string} s
  * @param {number} t
  * @return {number}
  */
 var lengthAfterTransformations = function(s, t) {
-        const cnt = new Array(26).fill(0);
-
-        for (const c of s) {
-            cnt[c.charCodeAt(0) - 'a'.charCodeAt(0)]++;
-        }
-
-        for (let j = 0; j < t; j++) {
-            const tmp = new Array(26).fill(0);
-            for (let i = 0; i < 26; i++) {
-                if (i === 25) {
-                    tmp[0] = (tmp[0] + cnt[i]) % MOD;
-                    tmp[1] = (tmp[1] + cnt[i]) % MOD;
-                } else {
-                    tmp[i + 1] = (tmp[i + 1] + cnt[i]) % MOD;
-                }
-            }
-            cnt.splice(0, 26, ...tmp); // Update cnt with tmp values
-        }
-
-        return cnt.reduce((len, c) => (len + c) % MOD, 0);
+    let f = new Array(26).fill(0);
+    const n = s.length;
+    for(let i = 0;i < n; i++) f[s.charCodeAt(i) - 97]++;
+    for(let j = 0; j < t; j++){
+        const c = new Array(26).fill(0);
+        c[0] = (c[0] + f[25]) % MOD;
+        c[1] = (c[1] + f[25]) % MOD;
+        for(let i = 1; i < 26; i++) c[i] = (f[i-1] + c[i]) % MOD;
+        f = c;
+    }
+    let res = 0;
+    for(let i = 0; i < 26; i++) res = (res + f[i]) % MOD;
+    return res;
 };
