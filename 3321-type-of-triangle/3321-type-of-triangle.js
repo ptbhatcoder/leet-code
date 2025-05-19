72695e4c71ece443fffd5019a722fdcf8c8c2ba4
Pranav Thirthahalli Bhat <ptbhatcoder@users.noreply.github.com>
2025-05-19 06:52:04 +0530

    Time: 0 ms (100%), Space: 55.2 MB (44.68%) - LeetHub

--- 3321-type-of-triangle/3321-type-of-triangle.js
+++ 3321-type-of-triangle/3321-type-of-triangle.js
@@ -5,7 +5,7 @@
 var triangleType = function(nums) {
     const [a, b, c] = nums;
     if(a === b && b === c) return "equilateral";
-    let max = a > b ? a > c ? a : c : b > c ? b : c;
+    let max = Math.max(...nums);
     if(max >= (a + b + c) / 2) return "none";
     if(a === b || b === c || c === a) return "isosceles";
     return "scalene";
