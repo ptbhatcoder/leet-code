6bf05504ec597fe5d1a5e46780eeb15ce1f75771
Pranav Thirthahalli Bhat <ptbhatcoder@users.noreply.github.com>
2025-05-20 08:50:42 +0530

    Time: 22 ms (64.15%), Space: 99.8 MB (9.43%) - LeetHub

--- 3639-zero-array-transformation-i/3639-zero-array-transformation-i.js
+++ 3639-zero-array-transformation-i/3639-zero-array-transformation-i.js
@@ -5,10 +5,10 @@
  */
 var isZeroArray = function(nums, queries) {
     const n = nums.length;
-    const times = new Array(n).fill(0);
+    const times = new Array(n + 1).fill(0);
     for(const [s, e] of queries){
         times[s]++;
-        if(e + 1 < n) times[e+1]--;
+        times[e+1]--;
     }
     for(let i = 0, runner = 0; i < n; i++){
         runner += times[i];
