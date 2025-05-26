756591c98b4d874239dc3d00a8acda48eec591c4
Pranav Thirthahalli Bhat <ptbhatcoder@users.noreply.github.com>
2025-05-26 06:53:52 +0530

    Time: 457 ms (96.08%), Space: 125.8 MB (94.12%) - LeetHub

--- 1986-largest-color-value-in-a-directed-graph/1986-largest-color-value-in-a-directed-graph.js
+++ 1986-largest-color-value-in-a-directed-graph/1986-largest-color-value-in-a-directed-graph.js
@@ -4,35 +4,39 @@
  * @return {number}
  */
 var largestPathValue = function(colors, edges) {
-    const n = colors.length;
-    const visited = Array(n).fill(0);
-    const maxCount = Array(n).fill(0).map(_=>Array(26).fill(0));
-    const adj = Array(n).fill(0).map(_=>[]);
-    
-    for(const [a,b] of edges){
-      adj[a].push(b);
-    }
-    let res = 0;
-    for(let i=0; i<n; i++){
-      res = Math.max(res, dfs(i, maxCount));
-    };
-    return res === Infinity ? -1 : res;
+    let graph = new Map();
+    let indegree = new Array(colors.length).fill(0);
+    let colorMap = new Map();
 
-    function dfs(node, colorsCount){
-      const cur = colors[node].charCodeAt()-97;
-      if(!visited[node]){
-        visited[node] = 1;
-        for(const next of adj[node]){
-          if(dfs(next, colorsCount) === Infinity){
-            return Infinity;
-          }
-          for(let k=0; k<26; k++){
-            maxCount[node][k] = Math.max(maxCount[node][k], maxCount[next][k]);
-          }
-        }
-        maxCount[node][cur]++;
-        visited[node] = 2;
-      }
-      return visited[node] === 2 ? maxCount[node][cur] : Infinity;
+    for(let [src,des] of edges){
+        if(!graph.has(src)) graph.set(src,[des]);
+        else graph.get(src).push(des);
+        indegree[des]++;
     }
+    let q = [];
+    for(let i=0;i<colors.length;i++){
+        if(indegree[i]===0) q.push(i);
+    }
+    let counter = 0;
+    let largestColor = 0;
+    const colorCount = Array.from({ length: colors.length }, () => new Array(26).fill(0));
+
+    while(q.length>0){
+        let node = q.shift();
+        let neis = graph.get(node) || [];
+        let colorIdx = colors.charCodeAt(node) - 97; 
+        colorCount[node][colorIdx]++;
+        largestColor = Math.max(largestColor, colorCount[node][colorIdx]);
+        counter++;
+        for(let nei of neis){
+            for (let c = 0; c < 26; c++) {
+                colorCount[nei][c] = Math.max(colorCount[nei][c], colorCount[node][c]);
+            }
+            indegree[nei]--;
+            if(indegree[nei]===0) q.push(nei);
+        }
+    }
+
+
+    return counter === colors.length ? largestColor : -1;
 };
