22e49d4c480ae304fafe9dcbe7922e181ddd6aea
Pranav Thirthahalli Bhat <ptbhatcoder@users.noreply.github.com>
2025-05-30 07:45:10 +0530

    Time: 42 ms (66.67%), Space: 72.7 MB (77.78%) - LeetHub

--- 2438-find-closest-node-to-given-two-nodes/2438-find-closest-node-to-given-two-nodes.js
+++ 2438-find-closest-node-to-given-two-nodes/2438-find-closest-node-to-given-two-nodes.js
@@ -1,21 +1,14 @@
-const getShortestDists = (node, g) => {
-    const dist = g.map(_ => Infinity);
-    dist[node] = 0;
-    let q = [node];
-    const visited = new Set;
-    while(q.length > 0){
-        const next = [];
-        for(const cur of q){
-            if(visited.has(cur)) continue;
-            visited.add(cur);
-            for(const neighbour of g[cur]){
-                if(!visited.has(neighbour)){
-                    dist[neighbour] = Math.min(dist[neighbour], dist[cur] + 1);
-                    next.push(neighbour);
-                }
-            }
-        }
-        q = next;
+const getShortestDists = (node, edges) => {
+    const dist = edges.map(_ => -1);
+    let cur = node;
+    let d = 0;
+    while(cur !== -1 && dist[cur] === -1){
+        dist[cur] = d;
+        d++;
+        cur = edges[cur];
+    }
+    for(let i = 0;i < dist.length; i++){
+        if(dist[i] < 0) dist[i] = Infinity;
     }
     return dist;
 }
@@ -27,13 +20,8 @@ const getShortestDists = (node, g) => {
  */
 var closestMeetingNode = function(edges, node1, node2) {
     const n = edges.length;
-    const g = Array.from({ length: n }, _ => []);
-    for(let s = 0; s < n;  s++){
-        const e = edges[s];
-        if(e > -1) g[s].push(e);
-    }
-    const d1 = getShortestDists(node1, g);
-    const d2 = getShortestDists(node2, g);
+    const d1 = getShortestDists(node1, edges);
+    const d2 = getShortestDists(node2, edges);
     let res = -1, smallest = Infinity;
     for(let i = 0; i < n; i++){
         const max = Math.max(d1[i], d2[i]);
