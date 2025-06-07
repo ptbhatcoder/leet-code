a8d95ca585390601254db7d62157b18c243123b5
Pranav Thirthahalli Bhat <ptbhatcoder@users.noreply.github.com>
2025-06-07 10:02:39 +0530

    Time: 327 ms (0%), Space: 64.3 MB (100%) - LeetHub

--- 3445-lexicographically-minimum-string-after-removing-stars/3445-lexicographically-minimum-string-after-removing-stars.js
+++ 3445-lexicographically-minimum-string-after-removing-stars/3445-lexicographically-minimum-string-after-removing-stars.js
@@ -5,25 +5,21 @@
 var clearStars = function(s) {
     const n = s.length;
     const p = {};
-    const exits = new Set;
+    const res = [...s];
     for(let i = 0; i < n; i++){
         const c  = s[i];
         if(c === '*'){
+            res[i] = ''
             for(const v of 'abcdefghijklmnopqrstuvwxyz'){
                 if(v in p && p[v].length > 0){
-                    exits.add(p[v].pop());
+                    res[p[v].pop()] = '';
                     break;
                 }
             }
-            exits.add(i);
         } else {
             if(!p[c]) p[c] = [];
             p[c].push(i);
         }
-    }
-    const res = [];
-    for(let i = 0; i < n; i++){
-        if(!exits.has(i)) res.push(s[i]);
     }
     return res.join('');
 };
