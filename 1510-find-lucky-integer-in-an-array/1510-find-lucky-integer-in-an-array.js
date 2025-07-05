929d8dd35af2395831a66442ab1ffc5e4eff60c5
Pranav Thirthahalli Bhat <ptbhatcoder@users.noreply.github.com>
2025-07-05 11:05:04 +0530

    Time: 6 ms (19.46%), Space: 56.2 MB (29.87%) - LeetHub

--- 1510-find-lucky-integer-in-an-array/1510-find-lucky-integer-in-an-array.js
+++ 1510-find-lucky-integer-in-an-array/1510-find-lucky-integer-in-an-array.js
@@ -6,6 +6,9 @@ var findLucky = function(arr) {
     const f = {};
     for(const c of arr)  f[c] = (f[c] || 0) + 1;
     let max = -1;
-    for(const c of arr) if(f[c] === c) max = Math.max(max, c);
+    for(const k of Object.keys(f)){
+        const c = +k;
+         if(f[k] === c) max = Math.max(max, c);
+    }
     return max;
 };
