d03063aa8ec644b8d92a143c27253a82f6dc9787
Pranav Thirthahalli Bhat <ptbhatcoder@users.noreply.github.com>
2025-08-19 06:29:12 +0530

    Time: 2 ms (100%), Space: 65 MB (100%) - LeetHub

--- 2432-number-of-zero-filled-subarrays/2432-number-of-zero-filled-subarrays.js
+++ 2432-number-of-zero-filled-subarrays/2432-number-of-zero-filled-subarrays.js
@@ -5,7 +5,9 @@
 var zeroFilledSubarray = function(nums) {
     let ans = 0;
     let span = 0;
-    for(const num of nums){
+    const n = nums.length;
+    for(let i = 0; i < n; i++){
+        const num = nums[i]
         if(num === 0){
             span++;
             ans += span;
