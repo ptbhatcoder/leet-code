92ea5e2ff6a83c950b23043effb4b7889bb4eca9
Pranav Thirthahalli Bhat <ptbhatcoder@users.noreply.github.com>
2025-10-20 08:37:24 +0530

    Time: 1 ms (50.59%), Space: 54.9 MB (74.58%) - LeetHub

--- 2137-final-value-of-variable-after-performing-operations/2137-final-value-of-variable-after-performing-operations.js
+++ 2137-final-value-of-variable-after-performing-operations/2137-final-value-of-variable-after-performing-operations.js
@@ -4,7 +4,7 @@
  */
 var finalValueAfterOperations = function(operations) {
     let diff = 0;
-    for(const op  of operations){
+    for(const op of operations){
         if(op.startsWith('+') || op.endsWith('+')) diff++;
         else diff--;
     }
