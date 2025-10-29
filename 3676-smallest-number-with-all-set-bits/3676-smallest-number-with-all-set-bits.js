3e9cea30a9d9555b75aa9e38214fa9cecf068b6c
Pranav Thirthahalli Bhat <ptbhatcoder@users.noreply.github.com>
2025-10-29 08:44:35 +0530

    Time: 0 ms (100%), Space: 55.5 MB (65.45%) - LeetHub

--- 3676-smallest-number-with-all-set-bits/3676-smallest-number-with-all-set-bits.js
+++ 3676-smallest-number-with-all-set-bits/3676-smallest-number-with-all-set-bits.js
@@ -1,15 +1,10 @@
-const count = val => {
-    let c = 0;
-    while(val > 0){
-        c++;
-        val >>= 1;
-    }
-    return c;
-}
+
 /**
  * @param {number} n
  * @return {number}
  */
 var smallestNumber = function(n) {
-    return (1 << count(n)) - 1;
+    const leadingZeroes = Math.clz32(n);
+    const digits = 32 - leadingZeroes;
+    return (1 << digits) - 1;
 };
