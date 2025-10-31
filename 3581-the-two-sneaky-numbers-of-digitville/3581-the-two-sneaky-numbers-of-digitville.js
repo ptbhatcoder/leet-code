919299490bd6779772dfc4d4eedb4ad8be2ac5a0
Pranav Thirthahalli Bhat <ptbhatcoder@users.noreply.github.com>
2025-10-31 06:39:03 +0530

    Time: 2 ms (56.25%), Space: 57.1 MB (53.75%) - LeetHub

---
+++ 3581-the-two-sneaky-numbers-of-digitville/3581-the-two-sneaky-numbers-of-digitville.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var getSneakyNumbers = function(nums) {
+    const ans = [];
+    const n = nums.length - 2;
+    for(const num of nums){
+        const key = num < 0 ? -num-1 : num;
+        if(nums[key] < 0) ans.push(key);
+        else nums[key] = -nums[key] - 1;
+    }
+    return ans;
+};
