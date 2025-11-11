56a9c64e2b3f1c18c675cb4faa980c95e1b98292
Pranav Thirthahalli Bhat <ptbhatcoder@users.noreply.github.com>
2025-11-11 07:05:33 +0530

    Time: 44 ms (92.38%), Space: 58.5 MB (62.86%) - LeetHub

--- 0474-ones-and-zeroes/0474-ones-and-zeroes.js
+++ 0474-ones-and-zeroes/0474-ones-and-zeroes.js
@@ -1,4 +1,3 @@
-
 /**
  * @param {string[]} strs
  * @param {number} m
@@ -6,34 +5,18 @@
  * @return {number}
  */
 var findMaxForm = function(strs, m, n) {
-    const map = {};
-    const l = strs.length;
-    const dp = (i, rm, rn) => {
-        if(i >= strs.length) return 0;
-
-        if(rm < 0 || rn < 0) return 0;
-        
-        const key = `${i}#m=${rm}#n=${rn}`;
-        
-        if(key in map) return map[key];
-
-        let ans = Infinity;
-        const exc = dp(i + 1, rm, rn);
-
+    const dp = Array.from({ length: 1 + m }, _ => Array.from({ length: 1 + n }, _ => 0));
+    for(const str of strs){
         let ones = 0, zeroes = 0;
-        for(const c of strs[i]){
+        for(const c of str){
             if(c === '1') ones++;
             else zeroes++;
         }
-
-        let inc = 0;
-        if(rm >= zeroes && rn >= ones){
-            inc = 1 + dp(i + 1, rm - zeroes, rn - ones);
+        for(let ze = m; ze >= zeroes; ze--){
+            for(let on = n; on >= ones; on--){
+                dp[ze][on] = Math.max(dp[ze][on], 1 + dp[ze - zeroes][on - ones]);
+            }
         }
-
-        ans = Math.max(inc, exc);
-        map[key] = ans;
-        return ans;
     }
-    return dp(0, m, n);
+    return dp[m][n];
 };
