be88695ca78512c601dcc592473a5d4e4ea8da0c
Pranav Thirthahalli Bhat <ptbhatcoder@users.noreply.github.com>
2025-11-19 06:38:20 +0530

    Time: 1 ms (50.56%), Space: 55.2 MB (61.11%) - LeetHub

--- 2274-keep-multiplying-found-values-by-two/2274-keep-multiplying-found-values-by-two.js
+++ 2274-keep-multiplying-found-values-by-two/2274-keep-multiplying-found-values-by-two.js
@@ -6,6 +6,6 @@
 var findFinalValue = function(nums, original) {
     const vals = new Set(nums);
     let cur = original;
-    while(vals.has(cur)) cur = cur << 1;
+    while(vals.has(cur)) cur <<= 1;
     return cur;
 };
