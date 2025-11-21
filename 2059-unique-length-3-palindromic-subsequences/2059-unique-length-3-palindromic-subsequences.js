0b875ef11405d2cc58799e70f8dbe605099f5df2
Pranav Thirthahalli Bhat <ptbhatcoder@users.noreply.github.com>
2025-11-21 06:34:55 +0530

    Time: 410 ms (12.96%), Space: 63.8 MB (61.11%) - LeetHub

--- 2059-unique-length-3-palindromic-subsequences/2059-unique-length-3-palindromic-subsequences.js
+++ 2059-unique-length-3-palindromic-subsequences/2059-unique-length-3-palindromic-subsequences.js
@@ -1,21 +1,20 @@
-const chars = 'abcdefghijklmnopqrstuvwxyz';
 /**
  * @param {string} s
  * @return {number}
  */
 var countPalindromicSubsequence = function(s) {
-    const l = {}, r = {};
+    const start = {}, end = {};
     const n = s.length;
     for(let i = 0; i < n; i++){
         const c = s[i];
-        if(!(c in l)) l[c] = i;
-        r[c] = i;
+        end[c] = i;
+        if(c in start) continue;
+        start[c] = i;
     }
-    let ans = 0;
-    for(const c in l){
-        const left = l[c];
-        const right = r[c];
-        ans += new Set(s.slice(left + 1, right)).size;
+    let ans = 0
+    for(const c in start){
+        const range = s.slice(start[c] + 1, end[c]);
+        ans += new Set(range).size;
     }
     return ans;
 };
