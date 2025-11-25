bea1c85c90f8bd10c3a2169fcd7b21bbba39d1c6
Pranav Thirthahalli Bhat <ptbhatcoder@users.noreply.github.com>
2025-11-25 07:52:27 +0530

    Time: 3 ms (31.43%), Space: 53.5 MB (90%) - LeetHub

--- 1015-smallest-integer-divisible-by-k/1015-smallest-integer-divisible-by-k.js
+++ 1015-smallest-integer-divisible-by-k/1015-smallest-integer-divisible-by-k.js
@@ -3,7 +3,6 @@
  * @return {number}
  */
 var smallestRepunitDivByK = function(k) {
-    if([k % 2, k % 5].includes(0)) return -1;
     let cur = 0;
     for(let digits = 1; digits <= k; digits++){
         cur = (cur * 10 + 1) % k;
