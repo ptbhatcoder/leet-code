efbe467a51422f857c3a7016c91873cbcdabe363
Pranav Thirthahalli Bhat <ptbhatcoder@users.noreply.github.com>
2025-12-15 08:34:37 +0530

    Time: 3 ms (80%), Space: 66.9 MB (20%) - LeetHub

--- 2110-number-of-smooth-descent-periods-of-a-stock/2110-number-of-smooth-descent-periods-of-a-stock.js
+++ 2110-number-of-smooth-descent-periods-of-a-stock/2110-number-of-smooth-descent-periods-of-a-stock.js
@@ -3,16 +3,12 @@
  * @return {number}
  */
 var getDescentPeriods = function(prices) {
-    let ans = 0;
+    let ans = 1;
     const n = prices.length;
-    for(let i = 0; i < n; i++){
-        if(i > 0 && prices[i] === prices[i-1] - 1){
-            cur++;
-            ans += cur;
-        } else {
-            cur = 1;
-            ans++
-        }
+    for(let i = 1, cur = 1; i < n; i++){
+        if(prices[i] === prices[i-1] - 1) cur++;
+        else cur = 1;
+        ans += cur;
     }
     return ans;
 };
